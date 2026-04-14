30571b69532086e52b616e913da1347c63223021
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 08:50:00 +0530

    Create variables.tf

---
+++ terraform/modules/sg/variables.tf
@@ -0,0 +1,17 @@
+##################################  Mandatory Variables  ####################################
+variable "sg_name"{}
+variable "sg_description" {}
+variable "vpc_id" {}
+
+### Tags
+variable "project" {}
+variable "env" {}
+variable "common_tags" {
+  type = map
+  default = {}
+}
+
+###################################  Default Variables   ########################################
+variable "sg_tags" {
+    default = {}
+}
