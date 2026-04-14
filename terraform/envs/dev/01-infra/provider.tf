b997aeb64b9df3bb97ca58243b085e1ae54544f9
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 12:13:22 +0530

    Update provider.tf

--- terraform/envs/dev/01-infra/provider.tf
+++ terraform/envs/dev/01-infra/provider.tf
@@ -1,1 +1,55 @@
+terraform {
+  # Minimum Terraform CLI version required
+  required_version = ">= 1.12.0"
 
+  # Required providers and version constraints
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"   # "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38.0"     # "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.5.0"
+    }    
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+
+  # Remote Backend
+  backend "s3" {}   
+}
+
+provider "aws" {
+  # AWS region to use for all resources (from variables)
+  region = var.region   # "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = "pharma"
+      Env         = "dev"
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
