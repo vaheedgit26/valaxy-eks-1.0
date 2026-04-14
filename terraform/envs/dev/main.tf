2d0941e32abcb9ad58847d450b34d08b4bad7538
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 09:26:43 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,1 +1,19 @@
+# VPC-MODULE Calling
+module "vpc" {
+  source = "../modules/vpc"
 
+  # All the counts should be same 
+  azs_count             = 2
+  public_subnet_count   = 2
+  private_subnet_count  = 2
+  database_subnet_count = 2
+
+  vpc_cidr             = "10.100.0.0/16"     
+  public_subnet_cidr   = ["10.100.1.0/24", "10.100.2.0/24"]
+  private_subnet_cidr  = ["10.100.11.0/24", "10.100.12.0/24"]
+  database_subnet_cidr = ["10.100.31.0/24", "10.100.32.0/24"]
+
+  project      = var.project
+  env          = var.env
+  common_tags  = local.common_tags
+}
