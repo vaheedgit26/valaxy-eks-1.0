5b007e312ef3c6ee642e86f5802d1c1c196c798f
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 11:43:44 +0530

    Update main.tf

--- terraform/envs/dev/00-s3/main.tf
+++ terraform/envs/dev/00-s3/main.tf
@@ -1,1 +1,9 @@
+# S3-MODULE Calling
+module "s3" {
+  source = "../../modules/s3"      # Give the path to S3 MODULE accordingly
 
+  s3_bucket_name = "tfstate-${var.project}-${var.env}-${var.region}"
+  project_name   = var.project
+  env            = var.env
+  region         = var.region
+}
