8fa3704ea934b14d2b76a6f3f34d5afc8325287d
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 12:37:52 +0530

    Update 07-iam.tf

--- terraform/envs/dev/01-infra/07-iam.tf
+++ terraform/envs/dev/01-infra/07-iam.tf
@@ -3,8 +3,9 @@ data "aws_caller_identity" "current" {}
 module "iam" {
   source = "../../../modules/iam"
 
-  project           = var.project                     # "pharma"
-  env               = var.env                         # "dev"
+  project   = var.project         # "pharma"
+  env       = var.env             # "dev"
+
   oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_provider_url = module.eks.oidc_provider_url
   aws_account_id    = data.aws_caller_identity.current.account_id
