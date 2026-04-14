429c1e7b19498c2cdba54f16725a631bb4d7b3b7
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 08:43:32 +0530

    Create main.tf

---
+++ terraform/modules/s3/main.tf
@@ -0,0 +1,48 @@
+#resource "random_string" "suffix" {
+#  length  = 6
+#  upper   = false
+#  special = false
+#}
+
+resource "aws_s3_bucket" "bucket" {
+  # bucket = "${var.s3_bucket_name}-${random_string.suffix.result}"
+
+  bucket = var.s3_bucket_name
+  force_destroy = true   # will be deleted forcefully
+
+  lifecycle {
+    prevent_destroy = false
+  }
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = var.env
+    Project     = var.project_name
+    Purpose     = "terraform-backend"
+    Terraform   = "true"
+  }
+}
+
+# This will incur some cost
+#resource "aws_s3_bucket_versioning" "bucket_versioning" {
+#  bucket = aws_s3_bucket.bucket.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
