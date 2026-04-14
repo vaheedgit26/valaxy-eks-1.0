ec4a46e88a318245e77edd27e4e3c57f44bd8a33
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 22:49:30 +0530

    Update 05-rds.tf

--- terraform/envs/dev/01-infra/05-rds.tf
+++ terraform/envs/dev/01-infra/05-rds.tf
@@ -6,7 +6,7 @@ module "rds" {
   subnet_ids            = module.vpc.database_subnet_ids       # (since vpc outputs as list, so [] not required)
   vpc_id                = module.vpc.vpc_id
   # eks_security_group_id = module.eks.cluster_security_group_id
-  rds_allowed_security_group_ids = [module.eks.cluster_security_group_id, module.vpc.bastion_host_sg_id]
+  rds_allowed_security_group_ids = [module.eks.cluster_security_group_id, module.bastion_sg.sg_id]
 
   db_name               = "pharmadb"
   db_username           = "pharmaadmin"
