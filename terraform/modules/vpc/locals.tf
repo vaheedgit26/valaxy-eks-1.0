4584e6e0bf19b8cbc39383d29c7f9efbe48b3315
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 07:35:11 +0530

    Update locals.tf

--- terraform/modules/vpc/locals.tf
+++ terraform/modules/vpc/locals.tf
@@ -1,4 +1,4 @@
 locals {
   azs = slice(data.aws_availability_zones.available.names, 0, var.azs_count)
-  resource_name = "${var.project_name}-${var.env}"
+  resource_name = "${var.project}-${var.env}"
 }
