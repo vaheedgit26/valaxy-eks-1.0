1366262a58e62d968ee14b2112a20c34dcd3995c
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 12:31:18 +0530

    Create delete-infra.sh

---
+++ terraform/envs/dev/01-infra/scripts/delete-infra.sh
@@ -0,0 +1,51 @@
+###########################################################
+# Usage: bash delete-infra.sh <project_name> <env> <region> #
+###########################################################
+# <project_name> <env> <region>
+#!/usr/bin/env bash
+set -e
+
+BUCKET=$(terraform -chdir=../../00-s3-create output -raw bucket_id)              
+ENV=$(terraform -chdir=../../00-s3-create output -raw env)                       
+REGION=$(terraform -chdir=../../00-s3-create output -raw region)                 
+PROJECT=$(terraform -chdir=../../00-s3-create output -raw project)
+
+echo """
+📄 Details:
+     PROJECT : ${PROJECT_NAME}
+     ENV     : ${ENV}
+     REGION  : ${REGION}
+     BUCKET  : ${BUCKET}
+"""
+
+# terraform init
+
+cd ..
+
+terraform destroy \
+  -var="project=$PROJECT" \
+  -var="env=$ENV" \
+  -var="region=$REGION" 
+  
+  #-var="s3_bucket_id=$BUCKET"
+  # -auto-approve
+
+
+# terraform plan -destroy \
+#  -var="project_name=expense" \
+#  -var="env=dev" \
+#  -var="region=ap-south-1" \
+#  -out=destroy.tfplan
+
+# terraform apply destroy.tfplan
+
+##############################################################################
+# In VPC :: 
+#   project_name
+#   env
+#   region
+   
+# In EKS ::
+#   project
+#   env
+#   region
