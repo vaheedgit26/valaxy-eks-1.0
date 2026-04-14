15216c4e56e03c1fb10f8c5b443ba5f5c2e34cb4
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 08:03:12 +0530

    Create variables.tf

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,41 @@
+################################################################
+#                    Mandatory variables                       # 
+################################################################
+variable "ami_id" {}
+variable "public_key_name" {}
+variable "sg_ids" { type = list(string) }
+variable "subnet_id" {}
+
+variable "project_name" {}
+variable "env" {}
+variable "common_tags" { type = map }
+
+#####################################################################################################
+#                Default setting variables, you can overwrite them while calling                    #
+#####################################################################################################
+variable "associate_public_ip_address" { default = false }
+variable "ec2_tags" { default = {} } 
+variable "iam_instance_profile" { default = null }
+variable "user_data" { default = null }
+variable "what_type_instance" { default = null }
+variable "is_eip_required" { default = false }
+
+variable "is_nat_instance" {
+  type = bool
+  default = false 
+}
+
+variable "root_volume_size" {
+  type = number
+  default = 8
+}
+
+#variable "instance_type" {
+#    default = "t2.micro"
+#    validation {
+#      condition     = contains(["t2.micro", "t2.small", "t2.medium"], var.instance_type)
+#      error_message = "Valid values for instance type are: t2.micro t2.small t2.medium"
+#    } 
+#}
+
+variable "instance_type" {}
