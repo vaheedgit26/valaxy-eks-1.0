cfd95005988dac5ffa9981f359951da3c1a62e33
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 12:11:34 +0530

    Update variables.tf

--- terraform/envs/dev/01-infra/variables.tf
+++ terraform/envs/dev/01-infra/variables.tf
@@ -1,8 +1,21 @@
-# project_name, env and region extracted from S3 Bucket 
+# project, env and region extracted from S3 Bucket 
 variable "region" {}          #  Actually for vpc creation, it takes region from provider block (For privider.tf) or from provider.tf
 variable "project" {}
 variable "env" {}
 
-########################################################
+#############################################################################################################
 variable "ami_id" {}
 variable "public_key_name" {}
+#############################################################################################################
+
+variable "db_password" {
+  description = "Master password for the RDS PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
+
+variable "jwt_secret" {
+  description = "JWT signing secret for the application"
+  type        = string
+  sensitive   = true
+}
