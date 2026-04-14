afa44c879c525b944a8ad58712ae08550dc6db12
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 12:17:26 +0530

    Create 05-rds.tf

---
+++ terraform/envs/dev/01-infra/05-rds.tf
@@ -0,0 +1,14 @@
+module "rds" {
+  source = "../../../modules/rds"
+
+  project               = var.project  # "pharma"
+  env                   = var.env      # "dev"
+  subnet_ids            = module.vpc.database_subnet_ids       # (since vpc outputs as list, so [] not required)
+  vpc_id                = module.vpc.vpc_id
+  # eks_security_group_id = module.eks.cluster_security_group_id
+  rds_allowed_security_group_ids = [module.eks.cluster_security_group_id, module.vpc.bastion_host_sg_id]
+
+  db_name               = "pharmadb"
+  db_username           = "pharmaadmin"
+  db_password           = var.db_password
+}
