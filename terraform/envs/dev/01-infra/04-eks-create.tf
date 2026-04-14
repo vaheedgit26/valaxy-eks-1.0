0157d94b9ab653b9eea29d54072527352f687824
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 15:14:08 +0530

    Update 04-eks-create.tf

--- terraform/envs/dev/01-infra/04-eks-create.tf
+++ terraform/envs/dev/01-infra/04-eks-create.tf
@@ -12,7 +12,7 @@ module "eks" {
   cluster_subnet_ids               = module.vpc.private_subnet_ids    # (since vpc outputs as list, so [] not required)
   cluster_endpoint_private_access  = true
   cluster_endpoint_public_access   = false
-  cluster_addl_security_group_ids  = [module.vpc.bastion_host_sg_id]  # This is additional cluster SG and the default cluster SG is intact
+  # cluster_addl_security_group_ids  = [module.vpc.bastion_host_sg_id]  # This is additional cluster SG and the default cluster SG is intact
 
   node_subnet_ids     = module.vpc.private_subnet_ids
   node_instance_types = ["t3.small"]
