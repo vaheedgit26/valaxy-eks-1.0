c507e4cdd2d5891c2e6fa0d9d0199f3da01db99e
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-14 09:39:43 +0530

    Update 02-bastion.tf

--- terraform/envs/dev/02-bastion.tf
+++ terraform/envs/dev/02-bastion.tf
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "bastion_internet" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.bastion_sg.sg_id                     # aws_security_group.sg_nat_instance.id
+  security_group_id = module.bastion_sg.sg_id                       # aws_security_group.sg_nat_instance.id
 }
 
 # Bastion Host
