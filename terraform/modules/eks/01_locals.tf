1b6a311d0ec433b873a17e0dd3c2b377f357f610
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-16 09:55:50 +0530

    Update 01_locals.tf

--- terraform/modules/eks/01_locals.tf
+++ terraform/modules/eks/01_locals.tf
@@ -2,5 +2,5 @@ locals {
   resource_name = "${var.project}-${var.env}"
 
   # Full EKS cluster name used for resource naming and tagging
-  eks_cluster_name = "${local.resource_name}-eks-cluster"         # Example: "retail-dev-eks-cluster"
+  # eks_cluster_name = "${local.resource_name}-eks-cluster"         # Example: "retail-dev-eks-cluster"
 }
