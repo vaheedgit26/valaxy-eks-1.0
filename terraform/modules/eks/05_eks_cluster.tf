dd99431bd418effacdf6b9ceb5c43e5dfccd30ef
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-16 09:58:10 +0530

    Update 05_eks_cluster.tf

--- terraform/modules/eks/05_eks_cluster.tf
+++ terraform/modules/eks/05_eks_cluster.tf
@@ -3,7 +3,7 @@
 # -------------------------------------------------------------------------------
 resource "aws_eks_cluster" "main" {
 
-  name     = "${var.eks_cluster_name}"
+  name     = "${cluster_name}"
   version  = var.cluster_version
   role_arn = aws_iam_role.eks_cluster.arn
 
