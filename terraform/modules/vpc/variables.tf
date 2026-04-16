aab207749af28efe64f3851c5434aa15fd66c709
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-16 09:51:42 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -3,7 +3,7 @@ variable "vpc_cidr" {}
 variable "project" {}
 variable "env" {}
 
-variable "eks_cluster_name" { default = null }   # For EKS public and private ELBs 
+variable "eks_cluster_name" { default = null }   # For EKS public and private tags for ELBs 
 
 variable "vpc_count" {
   type = number
