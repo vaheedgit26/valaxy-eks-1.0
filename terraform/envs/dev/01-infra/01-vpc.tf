a5315a61c372236ce6eb015e11648e83084cab67
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-16 09:59:47 +0530

    Update 01-vpc.tf

--- terraform/envs/dev/01-infra/01-vpc.tf
+++ terraform/envs/dev/01-infra/01-vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
   private_subnet_cidr  = ["10.100.11.0/24", "10.100.12.0/24"]
   database_subnet_cidr = ["10.100.31.0/24", "10.100.32.0/24"]
 
-  eks_cluster_name = local.eks_cluster_name
+  eks_cluster_name = local.eks_cluster_name    # For tagging purpose of Public and Private subnets
 
   project      = var.project
   env          = var.env
