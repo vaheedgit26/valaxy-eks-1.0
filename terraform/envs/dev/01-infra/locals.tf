6dd63b3735697aa8305ab2e02b67b22a2d0a0efd
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-17 10:22:59 +0530

    Update locals.tf

--- terraform/envs/dev/01-infra/locals.tf
+++ terraform/envs/dev/01-infra/locals.tf
@@ -5,4 +5,5 @@ locals {
     Terraform   = "true"
   }
   eks_cluster_name = "${var.project}-${var.env}-eks-cluster"
+  bastion_sg_id    = var.cluster_endpoint_public_access == false ? module.bastion_sg.sg_id : null
 }
