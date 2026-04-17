44527fb92afb9b8531804c9eada04ca69fdacddc
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-17 09:28:22 +0530

    Update 02_variables.tf

--- terraform/modules/eks/02_variables.tf
+++ terraform/modules/eks/02_variables.tf
@@ -38,7 +38,7 @@ variable "node_disk_size" {
   }
 }
 
-variable "bastion_sg_id"    { type = list(string) }
+variable "bastion_sg_id"    { default = null }
 # variable "node_ssh_public_key" { type = string }
 
 variable "desired_capacity" { type = number }
