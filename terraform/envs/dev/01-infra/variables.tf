a24d4be094ce192352999082601459294019d7fa
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-17 09:52:39 +0530

    Update variables.tf

--- terraform/envs/dev/01-infra/variables.tf
+++ terraform/envs/dev/01-infra/variables.tf
@@ -5,8 +5,11 @@ variable "env" {}
 
 #############################################################################################################
 variable "ami_id" {}
-variable "public_key_name" {}
+# variable "public_key_name" {}
 variable "bastion_sg_id" { default = null }
+
+variable "cluster_endpoint_private_access" {}
+variable "cluster_endpoint_public_access" {}
 #############################################################################################################
 
 variable "db_password" {
