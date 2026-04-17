91413536e1ef49b1ed986ab0422dd9efa6afb84c
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-17 09:13:50 +0530

    Update 05_eks_cluster.tf

--- terraform/modules/eks/05_eks_cluster.tf
+++ terraform/modules/eks/05_eks_cluster.tf
@@ -36,3 +36,14 @@ resource "aws_eks_cluster" "main" {
 
 }
 
+resource "aws_security_group_rule" "eks_api_from_bastion" {
+  count = var.cluster_endpoint_public_access == false ? 1 : 0
+
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  source_security_group_id = var.bastion_sg_id
+  description              = "Allow bastion to access EKS API"
+}
